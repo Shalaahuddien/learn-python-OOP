62cdfbd82b94aa047cd08d3019e51090ef22f9ba
Shalaahuddien Daffa <77540526+Shalaahuddien@users.noreply.github.com>
2023-03-26 22:33:57 +0700

    Learn

--- OOP2.py
+++ OOP2.py
@@ -1,12 +1,15 @@
 class Person:
-    def __init__(self, firstname, lastname, age, country):
+    def __init__(self, firstname ='Asab', lastname = 'Yetayeh', age = 250, country = 'Findland'):
         self.firstname = firstname
         self.lastname = lastname
         self.age = age
         self.country = country
     def get_person_info(self):
-        return f'He is {self.firstname} {self.lastname}. He is from {self.country}. He is {self.age}.'
+        return f'He is {self.firstname} {self.lastname}. He is from {self.country}. He is {self.age} years old.'
 
 p = Person('John', 'Doe', 25, 'UK')
 print(p)
 print(p.get_person_info())
+
+p2 = Person()
+print(p2.get_person_info())
